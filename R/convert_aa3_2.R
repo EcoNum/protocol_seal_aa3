f08fc615f6944c02b6a2ca4de3ca771069fcefb6
GuyliannEngels <guyliann.engels@umons.ac.be>
2018-10-18 13:54:04 +0200

    improve convert_aa3 and add seal file

--- R/convert_aa3_2.R
+++ R/convert_aa3_2.R
@@ -1,22 +1,25 @@
-#' Title
+#' Conversion of aa3 file txt and excell
 #'
-#' @param file_aa3_txt 
-#' @param file_aa3_xlsx 
+#' @param file_aa3_txt txt file
+#' @param file_aa3_xlsx xlsx file
 #'
-#' @return
+#' @return un objet converti
+#' @import stringr
+#' @import readr
+#' @import lubridate
+#' @import readxl
 #' @export
 #'
 #' @examples
-
+#'
+#' #TODO
+#'
 convert_aa3 <- function(file_aa3_txt, file_aa3_xlsx){
-  require(stringr)
-  require(readr)
-  
-  # Import metadata and extract informaation 
-  header_read <- readr::read_lines(file_aa3_txt, n_max = 13, 
+  # Import metadata and extract informaation
+  header_read <- readr::read_lines(file_aa3_txt, n_max = 13,
                                    locale = readr::locale(encoding = "LATIN1"))
   header <- stringr::str_extract_all(header_read,
-                                     "(-?µ?\\w+:?\\.?-?/?\\w*:?/?\\d*)")
+                                     "(-?\u03BC?\\w+:?\\.?-?/?\\w*:?/?\\d*)")
   
   # Rename list elements
   sapply(header, `[[`, 1) -> names(header)
@@ -30,11 +33,11 @@ convert_aa3 <- function(file_aa3_txt, file_aa3_xlsx){
   # Check_1
   header_control <- c("ANAL", "RUN", "DATE", "TIME", "OPER", "COMM", "TYPE",
                       "CHAN", "METH", "UNIT", "Base", "Gain", "Lamp")
-  length_list_element <- c(2,2,2,2,2,2,4,4,4,4,4,4,4) 
+  length_list_element <- c(2,2,2,2,2,2,4,4,4,4,4,4,4)
   
   if (sum(names(header) != header_control) != 0 |
       sum(lengths(header) != length_list_element) != 0) {
-    stop("Attention : Erreur durant l'importation ou l'extraction des métadonnées")
+    stop("attention : erreur durant l importation ou l extraction des metadonnees")
   }
   
   # Change NO3 in NOx
@@ -48,11 +51,11 @@ convert_aa3 <- function(file_aa3_txt, file_aa3_xlsx){
   concs <- paste(results, "conc", sep = "_")
   vals <- paste(results, "values", sep = "_")
   
-  # Change in data NPinorganique.ANL in inorganique 
+  # Change in data NPinorganique.ANL in inorganique
   header$ANAL[2][header$ANAL[2]  == "NPinorganique.ANL" ] <- "inorga"
   header$ANAL[2][header$ANAL[2]  == "NPorganique.ANL" ] <- "orga"
   
-  # Create a several list with the method's information
+  # Create a several list with the method information
   method <- list()
   for (i in 2:4) {
     method[[i - 1]] <- list(method = header$METH[i],
@@ -65,7 +68,7 @@ convert_aa3 <- function(file_aa3_txt, file_aa3_xlsx){
   
   # Create a list with the meta information
   meta <- list(sample = paste(sub("\\.RUN$", "", header$RUN[2]),
-                              header$ANAL[2],sep = "-"), 
+                              header$ANAL[2],sep = "-"),
                sample_date = lubridate::dmy_hms(paste(header$DATE[2],
                                                       header$TIME[2])),
                author = header$OPER[2],
@@ -79,15 +82,15 @@ convert_aa3 <- function(file_aa3_txt, file_aa3_xlsx){
   raw_data <- raw_data[ , -c(3, 5:7, 18, 19)]
   
   # Rename the columns in raw_data
-  names(raw_data) <- c("sample_id", "peak_number", "sample_type", 
-                       "date_time", stds[1], concs[1], vals[1], stds[2], 
+  names(raw_data) <- c("sample_id", "peak_number", "sample_type",
+                       "date_time", stds[1], concs[1], vals[1], stds[2],
                        concs[2], vals[2], stds[3], concs[3], vals[3])
   
   # recoding type of variable
   raw_data$sample_type <- as.factor(raw_data$sample_type)
   raw_data$date_time <- lubridate::dmy_hms(raw_data$date_time)
   
-  # Add units' informations
+  # Add units informations
   for (i in 1:3) {
     attr(raw_data[[stds[i]]], "units") <- method[[i]]$unit
     attr(raw_data[[concs[i]]], "units") <- method[[i]]$unit
@@ -101,8 +104,8 @@ convert_aa3 <- function(file_aa3_txt, file_aa3_xlsx){
   # SAMP contient des valeurs manquantes
   for (i in which(raw_data$sample_type == "SAMP")) {
     if (is.na(raw_data$project[i]) == TRUE) {
-      stop("Attention : Présence de valeurs manquantes dans la colonnes `project`, le fichier xlsx et txt ne correspondent pas entièrement.")
-    } 
+      stop("Attention : Presence de valeurs manquantes dans la colonnes project, le fichier xlsx et txt ne correspondent pas entierement.")
+    }
   }
   attr(raw_data, "method") <- method
   attr(raw_data, "metadata") <- meta
